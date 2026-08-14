bd247a35fc50d74afc9b1d06c751e467e9fb8a1c
Huzaifa Haider Khan <huzaifa.haider19@gmail.com>
2026-08-15 01:30:47 +0500

    feat: set up React Router and add page components

---
+++ frontend/src/pages/MyDocuments.jsx
@@ -0,0 +1,9 @@
+function MyDocuments() {
+  return (
+    <div>
+      <h1>My Documents</h1>
+    </div>
+  )
+}
+
+export default MyDocuments
---
+++ frontend/src/pages/UploadDocuments.jsx
@@ -0,0 +1,9 @@
+function UploadDocuments() {
+  return (
+    <div>
+      <h1>Upload Documents</h1>
+    </div>
+  )
+}
+
+export default UploadDocuments
